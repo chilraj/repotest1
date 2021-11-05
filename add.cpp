bde7ed0e344d20bd1808e18de891c216e0ac5731
chilraj <81753423+chilraj@users.noreply.github.com>
2021-11-05 08:01:20 +0530

    Update add.cpp
    added function add_int()

--- add.cpp
+++ add.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+int add_int(int a, int b)
+{
+  return a + b;
+}
+
 int main(){
   int a, b;
   int sum=0;
   a =10;
   b = 20;
-  sum - a + b;
+  //sum - a + b;
+  sum = add_int(a, b);
   cout << "sum = " << sum <<endl;
   return 1;
 }
